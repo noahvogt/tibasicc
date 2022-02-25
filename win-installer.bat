11d4bd43a085087574069606a0e1745e11bba466
Noah Vogt <noah@noahvogt.com>
2022-02-25 23:34:22 +0100

    fix windows install script as pointed out by #2 + update gitignore

--- win-installer.bat
+++ win-installer.bat
@@ -8,40 +8,39 @@ if not exist "%HOMEDRIVE%\Program Files (x86)\tibasicc\bin" (
 :: add g++ to path variable (only in this session)
 set PATH=%PATH%;C:\MinGW\bin\
 
-
 :: compiling
-echo g++ -g -Wall -pedantic  -c -o src/main.o src/main.cpp
-g++ -g -Wall -pedantic  -c -o src/main.o src/main.cpp || goto compileError
-echo g++ -g -Wall -pedantic   -c -o src/tokens.o src/tokens.cpp
-g++ -g -Wall -pedantic   -c -o src/tokens.o src/tokens.cpp || goto compileError
-echo g++ -g -Wall -pedantic   -c -o src/tibasic.o src/tibasic.cpp
-g++ -g -Wall -pedantic   -c -o src/tibasic.o src/tibasic.cpp || goto compileError
-echo g++ -o tibasicc src/main.o src/tibasic.o src/tokens.o
-g++ -o tibasicc src/main.o src/tibasic.o src/tokens.o
+echo g++ -g -Wall -pedantic  -c -o src/compiler.o src/compiler.cc
+g++ -g -Wall -pedantic  -c -o src/compiler.o src/compiler.cc || goto compileError
+echo g++ -g -Wall -pedantic   -c -o src/token_util.o src/token_util.cc
+g++ -g -Wall -pedantic   -c -o src/token_util.o src/token_util.cc || goto compileError
+echo g++ -g -Wall -pedantic   -c -o src/main.o src/main.cc
+g++ -g -Wall -pedantic   -c -o src/main.o src/main.cc || goto compileError
+echo g++ -o tibasicc src/compiler.o src/token_util.o src/main.o
+g++ -o tibasicc src/compiler.o src/token_util.o src/main.o
 
 :: move executable to the final directory
-copy tibasicc.exe "%HOMEDRIVE%\Program Files (x86)\tibasicc\bin" || goto unknownError
+copy tibasicc.exe "%HOMEDRIVE%\Program Files (x86)\tibasicc\bin" || goto movingError
 
 goto success
 
 :adminError
 echo.
-echo Error: 	TI-Basic could'nt be installed. 
-echo        	Make sure to run this programm with admin privileges.
+echo Error: 	TI-Basic couldn't be installed. 
+echo        	Make sure to run this program with admin privileges.
 echo.
 exit
 
 :compileError
 echo.
-echo Error:		TI-Basic could'nt be installed.
+echo Error:		TI-Basic couldn't be installed.
 echo			Compiling with g++ failed. Make sure that it is installed correctly.
 echo.
 exit
 
-:unknownError
+:movingError
 echo.
-echo Error:		TI-Basic could'nt be installed.
-echo			Compiling with g++ failed. Make sure that it is installed correctly.
+echo Error:		TI-Basic couldn't be installed.
+echo			Moving the binary to your program files failed. Make sure that you have appropriate permissions to this directory.
 echo.
 exit
 
